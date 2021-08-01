4c73242dde4e02d77b7eda9d7011886f13169dcc
mgbams <cmgbams@gmail.com>
2021-08-02 00:48:58 +0200

    added mail and emailList components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import EmailList from "./EmailList";
+import Mail from "./Mail";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-      <h1>Let's build gmail</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+
+        <div className="app_body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,15 @@ import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import SidebarOption from "./SidebarOption";
 import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { IconButton } from "@material-ui/core";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 function Sidebar() {
   return (
@@ -24,6 +33,26 @@ function Sidebar() {
       />
 
       <SidebarOption Icon={StarIcon} title="Starred" number={27} />
+
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={27} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={6} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" number={8} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={4} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={2} />
+
+      <div className="sidebar_footer">
+        <div className="sidebar_footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
