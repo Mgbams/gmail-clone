9d6144b0f5117d25adbd41bed4d445db7fc0d43a
mgbams <cmgbams@gmail.com>
2021-08-01 17:51:25 +0200

    Header right styled

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
 import MenuIcon from "@material-ui/icons/Menu";
-import { IconButton } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
@@ -22,7 +24,17 @@ function Header() {
         <input type="text" placeholder="Search mail" />
         <ArrowDropDownIcon className="header_inputCaret" />
       </div>
-      <div className="header_right"></div>
+      <div className="header_right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+
+        <Avatar />
+      </div>
     </div>
   );
 }
